dfa741ed03794ed06ebd04eadf6e9135110118e4
TheOnlyFastCoder2 <ponomareow.alex2018@gmail.com>
2025-12-15 17:24:52 +0400

    just save

--- src/_stm/react/Active.tsx
+++ src/_stm/react/Active.tsx
@@ -5,24 +5,33 @@ import type { Signal } from '..';
 const isUndefined = Symbol('undefined');
 type Sg = TRSignal<any> | Signal<any> | TRComputed<any>;
 interface ActiveProps<T> {
-  sg: Sg;
+  sg?: Sg | typeof isUndefined;
   triggers?: Sg[];
   is?: T | T[] | ((v: T) => boolean) | typeof isUndefined;
   callback?: (v: boolean) => void;
   children: React.ReactNode | (() => React.ReactNode);
+  ch?: React.ReactNode | (() => React.ReactNode);
 }
 
-function _Active<T>({ sg, is = isUndefined, triggers = [], children, callback }: ActiveProps<T>) {
+function _Active<T>({
+  sg = isUndefined,
+  is = isUndefined,
+  triggers = [],
+  children,
+  ch,
+  callback,
+}: ActiveProps<T>) {
+  const _ch = ch ?? children;
   const [visible, setVisible] = useState(false);
   const isRerender = is === isUndefined;
   useWatch(() => {
     for (let item of triggers) {
       item.v;
     }
-
+    if (sg === isUndefined) return setVisible(!visible);
     const val = sg.v;
-
     if (isRerender) return setVisible(!visible);
+
     let result = false;
 
     if (is === undefined) {
@@ -39,7 +48,7 @@ function _Active<T>({ sg, is = isUndefined, triggers = [], children, callback }:
     setVisible(result);
   }, [...triggers, is]);
 
-  return !isRerender ? (visible ? showChildren(children) : null) : showChildren(children);
+  return !isRerender ? (visible ? showChildren(_ch) : null) : showChildren(_ch);
 }
 export const Active = _Active;
 function showChildren(ch: React.ReactNode | (() => React.ReactNode)) {
